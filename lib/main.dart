2ea8653a1f11b62d354590b96fd0c0c124bc8f5e
michaelsandner <michaelsandy@gmx.de>
2023-01-22 11:48:18 +0100

    fix: Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Training Organizer'),
     );
   }
 }
