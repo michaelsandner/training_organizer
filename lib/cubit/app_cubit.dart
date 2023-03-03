161632a6b90f090e79184689a654e3565d4a89ad
michaelsandner <michaelsandy@gmx.de>
2023-03-03 21:34:08 +0100

    fix: Adapt email templates

--- lib/cubit/app_cubit.dart
+++ lib/cubit/app_cubit.dart
@@ -169,8 +169,8 @@ class AppCubit extends Cubit<AppState> {
       final String subject = Uri.encodeComponent(
           'Aufnahme für das Schimmtraining der Wasserwacht Langenzenn');
       final String body = Uri.encodeComponent('''hallo zusammen :)
-        leider hat es etwas gedauert aber nun freuen wir euch mitteilen zu dürfen, dass $foreName endlich bei uns im Schwimmtraining mitmachen darf.
-        Dafür würden wir $foreName gerne zum Schnuppern am XXX um 17:00 Uhr am Hallenbad Langenzenn einladen.
+        leider hat es etwas gedauert aber nun freuen wir uns euch mitteilen zu dürfen, dass $foreName endlich bei uns im Schwimmtraining mitmachen darf.
+        Dafür würden wir $foreName gerne zum Schnuppertraining am XXX um 17:00 Uhr am Hallenbad Langenzenn einladen.
         Wir treffen uns kurz vorher um ein paar Fragen auszutauschen und gemeinsam die Abläufe zu erklären.
         Bitte gebt uns bescheid ob ihr noch Interesse habt und ob ihr am Termin teilnehmen könnt.
 
@@ -180,6 +180,25 @@ class AppCubit extends Cubit<AppState> {
          ''');
       uri = Uri.parse('mailto:$email?subject=$subject&body=$body');
     }
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    }
+  }
+
+  Future<void> sendMailAccecptedToWaitingList(
+      String email, String foreName) async {
+    Uri uri = Uri.parse('mailto:$email');
+    final String subject =
+        Uri.encodeComponent('Bestätigung Aufnahme Warteliste OG Langenzenn');
+    final String body = Uri.encodeComponent('''hallo zusammen,
+        hiermit bestätigen wir euch die Aufnahme von $foreName auf unsere Warteliste zum .
+
+        Viele Grüße
+
+        Euer Wasserwacht-Team
+         ''');
+    uri = Uri.parse('mailto:$email?subject=$subject&body=$body');
+
     if (await canLaunchUrl(uri)) {
       await launchUrl(uri);
     }
