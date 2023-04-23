7f708dbd21c3c623cc3cbe6c9174b8ff45ef4696
michaelsandner <michaelsandy@gmx.de>
2023-04-23 15:38:10 +0200

    feat: Update email message

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -54,14 +54,17 @@ Future<void> sendMailToWaitingListTrainees(List<Trainee> trainees) async {
   final String body = Uri.encodeComponent('''hallo zusammen :)
         leider hat es etwas gedauert aber nun freuen wir uns euch mitteilen zu dürfen, dass ihr endlich bei uns im Schwimmtraining mitmachen dürft.
         Dafür würden wir euch gerne zum Schnuppertraining am XXX um 17:00 Uhr am Hallenbad Langenzenn einladen.
-        Wir treffen uns kurz vorher um euch ein paar Infos zum Trainingsablauf zu geben und den Kids das Hallenbad zu zeigen.
+        Das Training startet geht von 17-18 Uhr. Wir treffen uns allerdings schon kurz vorher um euch ein paar Infos zum Trainingsablauf zu geben und den Kids das Hallenbad zu zeigen.
         Bitte gebt uns bescheid ob ihr noch Interesse habt und ob ihr am Termin teilnehmen könnt.
+        (Falls ihr kein Interesse mehr am Schwimmtraining habt, bitte ebenfalls bescheid geben.)
+
+        PS: Falls ihr die Email mehrmals bekommt und sich mehrer Kinder von euch auf der Warteliste befinden, sind mehrere Kinder eingeladen ;)
 
         Viele Grüße
 
         Euer Wasserwacht-Team
          ''');
-  Uri uri = Uri.parse('mailto:$email?subject=$subject&body=$body');
+  Uri uri = Uri.parse('mailto:?bcc=$email&subject=$subject&body=$body');
 
   await _launchUri(uri);
 }
