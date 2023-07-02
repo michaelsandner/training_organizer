7589e6600cbd91160059eece12faf3c08a0afbf7
michaelsandner <michaelsandy@gmx.de>
2023-07-02 17:58:53 +0200

    Refactor trainee list

--- lib/view/trainee_view/trainee_list.dart
+++ lib/view/trainee_view/trainee_list.dart
@@ -1,12 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training_organizer/cubit/app_cubit.dart';
 import 'package:training_organizer/cubit/app_state.dart';
-import 'package:training_organizer/model/trainee.dart';
 import 'package:training_organizer/services/platform_service.dart';
-import 'package:training_organizer/view/edit_view/add_trainee.dart';
+import 'package:training_organizer/view/trainee_view/trainee_list_item.dart';
 
 class TraineeList extends StatefulWidget {
   @override
@@ -23,250 +20,17 @@ class _TraineeListState extends State<TraineeList> {
     final screenSize = MediaQuery.of(context).size;
     return BlocBuilder<AppCubit, AppState>(
       builder: (context, state) {
-        return SingleChildScrollView(
-          child: Table(
-            defaultColumnWidth: const IntrinsicColumnWidth(),
-            columnWidths: const <int, TableColumnWidth>{
-              0: IntrinsicColumnWidth(),
-              1: IntrinsicColumnWidth(),
-              2: IntrinsicColumnWidth(),
-              3: FlexColumnWidth(),
-              4: FlexColumnWidth(),
-              5: FlexColumnWidth(),
-              6: FlexColumnWidth(),
-            },
-            children:
-                List<TableRow>.generate(state.selectedTrainees.length, (index) {
-              final trainee = state.selectedTrainees[index];
-              return TableRow(
-                children: [
-                  _EmailButton(trainee: trainee),
-                  EditButton(
-                    refresh: refresh,
-                    trainee: trainee,
-                  ),
-                  UpAndDownButtons(refresh: refresh, trainee: trainee),
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: GestureDetector(
-                        onTap: () => showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Ausbildungen'),
-                                content: SizedBox(
-                                  height: 200,
-                                  child: Column(
-                                    children: List.generate(
-                                        trainee.badges.length, (index) {
-                                      final currentBadge =
-                                          trainee.badges[index];
-                                      if (currentBadge != null) {
-                                        return Row(
-                                          children: [
-                                            currentBadge.icon,
-                                            const SizedBox(
-                                              width: 10,
-                                            ),
-                                            if (currentBadge.date != null)
-                                              Text(currentBadge.date!.year
-                                                  .toString()),
-                                          ],
-                                        );
-                                      } else {
-                                        return const Icon(
-                                          Icons.warning,
-                                          color: Colors.red,
-                                        );
-                                      }
-                                    }),
-                                  ),
-                                ),
-                              );
-                            }),
-                        child: Row(
-                          children: [
-                            if (state.selectedGroup == FilterableGroup.all &&
-                                !isMobile(screenSize))
-                              Text(trainee.groupShortName),
-                            if (state.selectedGroup == FilterableGroup.all &&
-                                !isMobile(screenSize))
-                              const SizedBox(width: 10),
-                            SelectableText(
-                              trainee.surname,
-                              style: trainee.isMember
-                                  ? const TextStyle(color: Colors.black)
-                                  : const TextStyle(color: Colors.red),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            SelectableText(
-                              trainee.forename,
-                              style: trainee.isMember
-                                  ? const TextStyle(color: Colors.black)
-                                  : const TextStyle(color: Colors.red),
-                            ),
-                            if (trainee.comment != "")
-                              Tooltip(
-                                message: trainee.comment,
-                                child: const Padding(
-                                  padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                  child: Icon(
-                                    Icons.warning,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  if (!isMobile(screenSize))
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: Row(
-                        children: List.generate(trainee.badges.length, (index) {
-                          final currentBadge = trainee.badges[index];
-                          if (currentBadge != null) {
-                            return Tooltip(
-                              message: currentBadge.date == null
-                                  ? '${currentBadge.fullName} \n Datum: - '
-                                  : '${currentBadge.fullName} \n Datum: ${currentBadge.date!.year.toString()}',
-                              child: currentBadge.icon,
-                            );
-                          } else {
-                            return const Icon(
-                              Icons.warning,
-                              color: Colors.red,
-                            );
-                          }
-                        }),
-                      ),
-                    ),
-                  if (!isMobile(screenSize))
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: SelectableText(trainee.dateOfBirth),
-                    ),
-                  if (!isMobile(screenSize))
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: SelectableText(trainee.registrationDate),
-                    ),
-                  if (!isMobile(screenSize))
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: SelectableText(trainee.email),
-                    ),
-                  if (!isMobile(screenSize))
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: SelectableText(trainee.phone),
-                    ),
-                ],
-              );
-            }),
-          ),
+        return ListView.builder(
+          itemCount: state.selectedTrainees.length,
+          itemBuilder: (BuildContext context, int index) {
+            return TraineeListItem(
+              trainee: state.selectedTrainees[index],
+              refresh: refresh,
+              isLargeView: !isMobile(screenSize),
+            );
+          },
         );
       },
-    );
-  }
-}
-
-class _EmailButton extends StatelessWidget {
-  final Trainee trainee;
-  const _EmailButton({
-    required this.trainee,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final cubit = context.read<AppCubit>();
-
-    return IconButton(
-      onPressed: () => cubit.sendMailToTrainee(trainee),
-      icon: const Icon(Icons.mail),
-      color: Colors.blue,
-    );
-  }
-}
-
-class EditButton extends StatefulWidget {
-  final Trainee trainee;
-  final Function() refresh;
-  const EditButton({required this.trainee, required this.refresh});
-
-  @override
-  State<EditButton> createState() => _EditButtonState();
-}
-
-class _EditButtonState extends State<EditButton> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-
-  @override
-  void initState() {
-    _emailController.text = widget.trainee.email;
-    _phoneController.text = widget.trainee.phone;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => AddTrainee(
-                    trainee: widget.trainee,
-                  ))),
-      icon: const Icon(Icons.edit),
-      color: Colors.orange,
-    );
-  }
-}
-
-class UpAndDownButtons extends StatelessWidget {
-  final Function() refresh;
-  final Trainee trainee;
-  const UpAndDownButtons({
-    required this.refresh,
-    required this.trainee,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final cubit = context.read<AppCubit>();
-    return Row(
-      children: [
-        IconButton(
-          onPressed: cubit.isUpgradePossible(trainee)
-              ? () {
-                  cubit.upgradeTrainee(trainee);
-                  refresh();
-                }
-              : null,
-          icon: const Icon(Icons.upgrade_sharp),
-          color: Colors.green,
-        ),
-        Transform.rotate(
-          angle: pi,
-          child: IconButton(
-            onPressed: cubit.isDowngradePossible(trainee)
-                ? () {
-                    cubit.downgradeTrainee(trainee);
-                    refresh();
-                  }
-                : null,
-            icon: const Icon(Icons.upgrade_sharp),
-            color: Colors.red,
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/view/trainee_view/trainee_list_item.dart
@@ -0,0 +1,274 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:training_organizer/cubit/app_cubit.dart';
+import 'package:training_organizer/model/trainee.dart';
+import 'package:training_organizer/view/edit_view/add_trainee.dart';
+
+class TraineeListItem extends StatelessWidget {
+  final Trainee trainee;
+  final Function refresh;
+  final bool isLargeView;
+  const TraineeListItem({
+    required this.trainee,
+    required this.refresh,
+    this.isLargeView = true,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return isLargeView
+        ? SizedBox(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                UpAndDownButtons(refresh: () => refresh, trainee: trainee),
+                ItemTextBox(
+                  text: '${trainee.forename} ${trainee.surname}',
+                  isMember: trainee.isMember,
+                  width: 200,
+                ),
+                ItemTextBox(
+                  text: trainee.dateOfBirth,
+                  isMember: trainee.isMember,
+                  width: 120,
+                ),
+                EditButton(trainee: trainee),
+                _EmailButton(trainee: trainee),
+                _BadgeOverlay(
+                    trainee: trainee, child: _Badges(trainee: trainee)),
+              ],
+            ),
+          )
+        : SizedBox(
+            height: 40,
+            child: Row(
+              children: [
+                UpAndDownButtons(refresh: () => refresh, trainee: trainee),
+                Expanded(
+                  child: _BadgeOverlay(
+                    trainee: trainee,
+                    child: ItemTextBox(
+                      text: '${trainee.forename} ${trainee.surname}',
+                      isMember: trainee.isMember,
+                    ),
+                  ),
+                ),
+                EditButton(trainee: trainee),
+                _EmailButton(trainee: trainee),
+              ],
+            ),
+          );
+  }
+}
+
+class _BadgeOverlay extends StatelessWidget {
+  final Trainee trainee;
+  final Widget child;
+  const _BadgeOverlay({
+    required this.child,
+    required this.trainee,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Ausbildungen'),
+              content: SizedBox(
+                height: 200,
+                child: Column(
+                  children: List.generate(trainee.badges.length, (index) {
+                    final currentBadge = trainee.badges[index];
+                    if (currentBadge != null) {
+                      return Row(
+                        children: [
+                          currentBadge.icon,
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          if (currentBadge.date != null)
+                            Text(currentBadge.date!.year.toString()),
+                        ],
+                      );
+                    } else {
+                      return const Icon(
+                        Icons.warning,
+                        color: Colors.red,
+                      );
+                    }
+                  }),
+                ),
+              ),
+            );
+          }),
+      child: child,
+    );
+  }
+}
+
+class _Badges extends StatelessWidget {
+  final Trainee trainee;
+  const _Badges({
+    required this.trainee,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: List.generate(trainee.badges.length, (index) {
+          final currentBadge = trainee.badges[index];
+          if (currentBadge != null) {
+            return Tooltip(
+              message: currentBadge.date == null
+                  ? '${currentBadge.fullName} \n Datum: - '
+                  : '${currentBadge.fullName} \n Datum: ${currentBadge.date!.year.toString()}',
+              child: currentBadge.icon,
+            );
+          } else {
+            return const Icon(
+              Icons.warning,
+              color: Colors.red,
+            );
+          }
+        }),
+      ),
+    );
+  }
+}
+
+class ItemTextBox extends StatelessWidget {
+  final String text;
+  final double? width;
+  final bool isMember;
+  const ItemTextBox({
+    required this.text,
+    this.width,
+    required this.isMember,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 3),
+      width: width,
+      child: SelectableText(
+        text,
+        style: TextStyle(
+            color: isMember ? Colors.black : Colors.red,
+            fontStyle: !isMember ? FontStyle.italic : FontStyle.normal),
+      ),
+    );
+  }
+}
+
+class _EmailButton extends StatelessWidget {
+  final Trainee trainee;
+  const _EmailButton({
+    required this.trainee,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<AppCubit>();
+
+    return IconButton(
+      padding: const EdgeInsets.all(0),
+      onPressed: () => cubit.sendMailToTrainee(trainee),
+      icon: const Icon(Icons.mail),
+      color: Colors.blue,
+    );
+  }
+}
+
+class EditButton extends StatefulWidget {
+  final Trainee trainee;
+
+  const EditButton({required this.trainee});
+
+  @override
+  State<EditButton> createState() => _EditButtonState();
+}
+
+class _EditButtonState extends State<EditButton> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+
+  @override
+  void initState() {
+    _emailController.text = widget.trainee.email;
+    _phoneController.text = widget.trainee.phone;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      padding: const EdgeInsets.all(0),
+      onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => AddTrainee(
+                    trainee: widget.trainee,
+                  ))),
+      icon: const Icon(Icons.edit),
+      color: Colors.orange,
+    );
+  }
+}
+
+class UpAndDownButtons extends StatelessWidget {
+  final Function() refresh;
+  final Trainee trainee;
+  const UpAndDownButtons({
+    required this.refresh,
+    required this.trainee,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<AppCubit>();
+    return Row(
+      children: [
+        IconButton(
+          iconSize: 30,
+          padding: const EdgeInsets.all(0),
+          onPressed: cubit.isUpgradePossible(trainee)
+              ? () {
+                  cubit.upgradeTrainee(trainee);
+                  refresh();
+                }
+              : null,
+          icon: const Icon(Icons.upgrade_sharp),
+          color: Colors.green,
+        ),
+        Transform.rotate(
+          angle: pi,
+          child: IconButton(
+            iconSize: 30,
+            padding: const EdgeInsets.all(0),
+            onPressed: cubit.isDowngradePossible(trainee)
+                ? () {
+                    cubit.downgradeTrainee(trainee);
+                    refresh();
+                  }
+                : null,
+            icon: const Icon(Icons.upgrade_sharp),
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+}
