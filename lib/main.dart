3e2af14ed9b6133f28867e7c16604a91144c78c9
michaelsandner <michaelsandy@gmx.de>
2023-10-02 10:54:49 +0200

    fix: Remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -28,15 +27,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Training Organizer',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: const MyHomePage(title: 'Training Organizer'),
@@ -47,16 +37,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -66,18 +46,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
           bottom: const TabBar(tabs: [
             Tab(
@@ -95,14 +67,15 @@ class _MyHomePageState extends State<MyHomePage> {
           ]),
         ),
         body: Center(
-          child: TabBarView(children: [
-            const TraineeView(),
-            PdfView(),
-            const StatisticsView()
-          ]),
+          child: TabBarView(
+            children: [
+              const TraineeView(),
+              PdfView(),
+              const StatisticsView(),
+            ],
+          ),
         ),
       ),
     );
-    // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
