04896b7d839800cd21e3d241cee8db8bae7715fc
michaelsandner <michaelsandy@gmx.de>
2023-10-14 09:42:54 +0200

    feat: Create and use new filter service

---
+++ lib/services/trainees_filter_service.dart
@@ -0,0 +1,30 @@
+import 'package:training_organizer/cubit/app_state.dart';
+import 'package:training_organizer/model/trainee.dart';
+
+class TraineesFilterService {
+  static List<Trainee> getTraineesOfGroup(List<Trainee> trainees, Group group) {
+    return trainees.where((element) => element.trainingGroup == group).toList();
+  }
+
+  static List<Trainee> getAllSaturdayTrainees(List<Trainee> trainees) {
+    return trainees
+        .where((element) =>
+            element.trainingGroup == Group.group1 ||
+            element.trainingGroup == Group.group2 ||
+            element.trainingGroup == Group.group3 ||
+            element.trainingGroup == Group.group4 ||
+            element.trainingGroup == Group.group5)
+        .toList();
+  }
+
+  static List<Trainee> getAllTrainers(List<Trainee> trainees) {
+    return trainees.where((element) => element.isTrainer).toList();
+  }
+
+  static int getCountOfTraineesWithQualification(
+      List<Trainee> trainees, String qualification) {
+    return trainees
+        .where((element) => element.hasQualification(qualification))
+        .length;
+  }
+}
