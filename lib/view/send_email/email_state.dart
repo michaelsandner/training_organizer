4a165e275de41e18a37e547f07e6566146fc6730
michaelsandner <michaelsandy@gmx.de>
2026-01-30 14:01:38 +0100

    Improve email page

--- lib/view/send_email/email_state.dart
+++ lib/view/send_email/email_state.dart
@@ -13,60 +13,111 @@ enum EmailList1 {
 }
 
 class EmailState with EquatableMixin {
-  final bool shouldSendToSaturdayKids;
-  final bool shouldSendToWednesdayKids;
-  final bool shouldSendToTrainer;
+  final bool shouldSendToSaturdayBlock5;
+  final bool shouldSendToSaturdayBlock1;
+  final bool shouldSendToSaturdayBlock2;
+  final bool shouldSendToSaturdayBlock4;
+  final bool shouldSendToAllSaturday;
+
+  final bool shouldSendToWednesdayBlock1;
+  final bool shouldSendToWednesdayBlock2;
   final bool shouldSendToActive;
-  final bool shouldSendToLeasure;
+  final bool shouldSendToLeisure;
+  final bool shouldSendToAllWednesday;
+
+  final bool shouldSendToTrainer;
+  final bool shouldSendToWaitinglist;
   final bool shouldSendToInvited;
 
   const EmailState({
-    required this.shouldSendToSaturdayKids,
-    required this.shouldSendToWednesdayKids,
-    required this.shouldSendToActive,
-    required this.shouldSendToLeasure,
+    required this.shouldSendToSaturdayBlock5,
+    required this.shouldSendToSaturdayBlock1,
+    required this.shouldSendToSaturdayBlock2,
+    required this.shouldSendToSaturdayBlock4,
+    required this.shouldSendToWednesdayBlock1,
+    required this.shouldSendToWednesdayBlock2,
     required this.shouldSendToTrainer,
+    required this.shouldSendToActive,
+    required this.shouldSendToLeisure,
     required this.shouldSendToInvited,
+    required this.shouldSendToWaitinglist,
+    required this.shouldSendToAllSaturday,
+    required this.shouldSendToAllWednesday,
   });
 
   factory EmailState.initial() {
     return const EmailState(
-      shouldSendToSaturdayKids: false,
-      shouldSendToWednesdayKids: false,
-      shouldSendToActive: false,
+      shouldSendToSaturdayBlock5: false,
+      shouldSendToSaturdayBlock1: false,
+      shouldSendToSaturdayBlock2: false,
+      shouldSendToSaturdayBlock4: false,
+      shouldSendToWednesdayBlock1: false,
+      shouldSendToWednesdayBlock2: false,
       shouldSendToTrainer: false,
+      shouldSendToActive: false,
+      shouldSendToLeisure: false,
       shouldSendToInvited: false,
-      shouldSendToLeasure: false,
+      shouldSendToWaitinglist: false,
+      shouldSendToAllSaturday: false,
+      shouldSendToAllWednesday: false,
     );
   }
 
   EmailState copyWith({
-    bool? shouldSendToSaturdayKids,
-    bool? shouldSendToWednesdayKids,
+    bool? shouldSendToSaturdayBlock5,
+    bool? shouldSendToSaturdayBlock1,
+    bool? shouldSendToSaturdayBlock2,
+    bool? shouldSendToSaturdayBlock4,
+    bool? shouldSendToWednesdayBlock1,
+    bool? shouldSendToWednesdayBlock2,
     bool? shouldSendToTrainer,
     bool? shouldSendToActive,
-    bool? shouldSendToLeasure,
+    bool? shouldSendToLeisure,
     bool? shouldSendToInvited,
+    bool? shouldSendToWaitinglist,
+    bool? shouldSendToAllSaturday,
+    bool? shouldSendToAllWednesday,
   }) {
     return EmailState(
-      shouldSendToSaturdayKids:
-          shouldSendToSaturdayKids ?? this.shouldSendToSaturdayKids,
-      shouldSendToWednesdayKids:
-          shouldSendToWednesdayKids ?? this.shouldSendToWednesdayKids,
+      shouldSendToSaturdayBlock5:
+          shouldSendToSaturdayBlock5 ?? this.shouldSendToSaturdayBlock5,
+      shouldSendToSaturdayBlock1:
+          shouldSendToSaturdayBlock1 ?? this.shouldSendToSaturdayBlock1,
+      shouldSendToSaturdayBlock2:
+          shouldSendToSaturdayBlock2 ?? this.shouldSendToSaturdayBlock2,
+      shouldSendToSaturdayBlock4:
+          shouldSendToSaturdayBlock4 ?? this.shouldSendToSaturdayBlock4,
+      shouldSendToWednesdayBlock1:
+          shouldSendToWednesdayBlock1 ?? this.shouldSendToWednesdayBlock1,
+      shouldSendToWednesdayBlock2:
+          shouldSendToWednesdayBlock2 ?? this.shouldSendToWednesdayBlock2,
       shouldSendToTrainer: shouldSendToTrainer ?? this.shouldSendToTrainer,
       shouldSendToActive: shouldSendToActive ?? this.shouldSendToActive,
-      shouldSendToLeasure: shouldSendToLeasure ?? this.shouldSendToLeasure,
+      shouldSendToLeisure: shouldSendToLeisure ?? this.shouldSendToLeisure,
       shouldSendToInvited: shouldSendToInvited ?? this.shouldSendToInvited,
+      shouldSendToWaitinglist:
+          shouldSendToWaitinglist ?? this.shouldSendToWaitinglist,
+      shouldSendToAllSaturday:
+          shouldSendToAllSaturday ?? this.shouldSendToAllSaturday,
+      shouldSendToAllWednesday:
+          shouldSendToAllWednesday ?? this.shouldSendToAllWednesday,
     );
   }
 
   @override
   List<Object?> get props => [
-        shouldSendToActive,
-        shouldSendToInvited,
-        shouldSendToLeasure,
-        shouldSendToSaturdayKids,
+        shouldSendToSaturdayBlock5,
+        shouldSendToSaturdayBlock1,
+        shouldSendToSaturdayBlock2,
+        shouldSendToSaturdayBlock4,
+        shouldSendToWednesdayBlock1,
+        shouldSendToWednesdayBlock2,
         shouldSendToTrainer,
-        shouldSendToWednesdayKids,
+        shouldSendToActive,
+        shouldSendToLeisure,
+        shouldSendToInvited,
+        shouldSendToLeisure,
+        shouldSendToAllSaturday,
+        shouldSendToAllWednesday
       ];
 }
---
+++ lib/view/send_email/send_email_page.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:training_organizer/cubit/app_cubit.dart';
+import 'package:training_organizer/view/send_email/email_cubit.dart';
+import 'package:training_organizer/view/send_email/email_state.dart';
+
+class SendEmailPage extends StatelessWidget {
+  const SendEmailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<EmailCubit, EmailState>(
+      builder: (context, state) {
+        final emailCubit = context.read<EmailCubit>();
+        return ListView(
+          children: [
+            const Text(
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                'Email senden an:'),
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      const Text('Samstag'),
+                      EmailListTile(
+                        title: 'Alle',
+                        value: state.shouldSendToAllSaturday,
+                        onChanged: emailCubit.shouldSendToAllSaturday,
+                      ),
+                      EmailListTile(
+                        title: 'Block 5 (Pipsy)',
+                        value: state.shouldSendToSaturdayBlock5,
+                        onChanged: emailCubit.shouldSendToSaturdayBlock5,
+                      ),
+                      EmailListTile(
+                        title: 'Block 1 (Elli)',
+                        value: state.shouldSendToSaturdayBlock1,
+                        onChanged: emailCubit.shouldSendToSaturdayBlock1,
+                      ),
+                      EmailListTile(
+                        title: 'Block 2 (Eddie)',
+                        value: state.shouldSendToSaturdayBlock2,
+                        onChanged: emailCubit.shouldSendToSaturdayBlock2,
+                      ),
+                      EmailListTile(
+                        title: 'Samstag: Block 4 (Ebbi)',
+                        value: state.shouldSendToSaturdayBlock4,
+                        onChanged: emailCubit.shouldSendToSaturdayBlock4,
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                    child: Column(
+                  children: [
+                    const Text('Mittwoch'),
+                    EmailListTile(
+                      title: 'Alle',
+                      value: state.shouldSendToAllWednesday,
+                      onChanged: emailCubit.shouldSendToAllWednesday,
+                    ),
+                    EmailListTile(
+                      title: 'Block 1 (Jugend I)',
+                      value: state.shouldSendToWednesdayBlock1,
+                      onChanged: emailCubit.shouldSendToWednesdayBlock1,
+                    ),
+                    EmailListTile(
+                      title: 'Block 2 (Jugend II)',
+                      value: state.shouldSendToWednesdayBlock2,
+                      onChanged: emailCubit.shouldSendToWednesdayBlock2,
+                    ),
+                    EmailListTile(
+                      title: 'Aktiv',
+                      value: state.shouldSendToActive,
+                      onChanged: emailCubit.shouldSendToActive,
+                    ),
+                    EmailListTile(
+                      title: 'Freizeit',
+                      value: state.shouldSendToLeisure,
+                      onChanged: emailCubit.shouldSendToLeisure,
+                    ),
+                  ],
+                ))
+              ],
+            ),
+            EmailListTile(
+              title: 'Trainer',
+              value: state.shouldSendToTrainer,
+              onChanged: emailCubit.shouldSendToTrainer,
+            ),
+            EmailListTile(
+              title: 'Warteliste',
+              value: state.shouldSendToWaitinglist,
+              onChanged: emailCubit.shouldSendToWaitinglist,
+            ),
+            EmailListTile(
+              title: 'Eingeladen zum Training',
+              value: state.shouldSendToInvited,
+              onChanged: emailCubit.shouldSendToInvited,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(width: 45),
+                TextButton(
+                  onPressed: () {
+                    final trainees = context.read<AppCubit>().state.trainees;
+                    emailCubit.sendEmail(trainees);
+                  },
+                  child: const Text(style: TextStyle(fontSize: 20), 'Senden'),
+                )
+              ],
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class EmailListTile extends StatelessWidget {
+  final String title;
+  final bool value;
+  final Function(bool) onChanged;
+
+  const EmailListTile({
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<EmailCubit, EmailState>(
+      builder: (context, state) {
+        return Material(
+          child: CheckboxListTile(
+            title: Text(title),
+            value: value,
+            onChanged: (bool? newValue) {
+              onChanged(newValue ?? false);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
