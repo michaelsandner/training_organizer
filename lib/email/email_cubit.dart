e7657b3b65fcccb79edb10e97bbee8b1bae977e1
michaelsandner <michaelsandy@gmx.de>
2026-01-31 14:21:18 +0100

    Rename files

--- lib/email/email_cubit.dart
+++ lib/email/email_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training_organizer/cubit/app_state.dart';
-import 'package:training_organizer/email/email_service.dart';
+import 'package:training_organizer/email/email_repository.dart';
 import 'package:training_organizer/email/email_state.dart';
 import 'package:training_organizer/model/trainee.dart';
 import 'package:training_organizer/services/trainees_filter_service.dart';
