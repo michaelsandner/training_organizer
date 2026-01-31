0195d2fa6cb0109d96a9312fd6717993ebb98ba4
michaelsandner <michaelsandy@gmx.de>
2026-01-31 14:19:41 +0100

    Move template strings to new file

---
+++ lib/email/template_strings.dart
@@ -0,0 +1,28 @@
+String getWaitingListAcceptSubject() =>
+    'Aufnahme Warteliste der Wasserwacht Langenzenn';
+String getWaitingListAcceptContent(String foreName, String registrationDate) =>
+    '''hallo zusammen,
+        hiermit bestätigen wir die Aufnahme von $foreName auf unsere Warteliste zum $registrationDate.    
+
+        Viele Grüße
+
+        Euer Wasserwacht-Team
+         ''';
+
+String getInvitedInvitationSubject() =>
+    'Aufnahme für das Schimmtraining der Wasserwacht Langenzenn';
+String getInvitedInvitationContent() => '''hallo zusammen :) 
+
+        leider hat es etwas gedauert aber nun freuen wir uns euch mitteilen zu dürfen, dass ihr endlich bei uns im Schwimmtraining mitmachen dürft.
+        Dafür würden wir euch gerne zum Schnuppertraining am XXX um 15:50 Uhr am Hallenbad Wilhermsdorf einladen.
+        Das Training findet von 16-17 Uhr statt. Wir treffen uns allerdings schon kurz vorher um euch ein paar Infos zum Trainingsablauf zu geben und um den Kids das Hallenbad zu zeigen.
+
+        Bitte gebt uns bescheid ob ihr noch Interesse habt und ob ihr am Termin teilnehmen könnt.
+        (Falls ihr kein Interesse mehr am Schwimmtraining habt, bitte ebenfalls bescheid geben.)
+
+        PS: Falls ihr die Email mehrmals bekommt und sich mehrer Kinder von euch auf der Warteliste befinden, sind mehrere Kinder eingeladen ;)
+
+        Viele Grüße
+
+        Euer Wasserwacht-Team
+         ''';
