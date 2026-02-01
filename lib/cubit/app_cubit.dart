8eb5d6824789f8ac745917a1727399c511e97be7
michaelsandner <michaelsandy@gmx.de>
2026-02-01 11:15:49 +0100

    create new subfolder

--- lib/cubit/app_cubit.dart
+++ lib/cubit/app_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:training_organizer/cubit/app_state.dart';
-import 'package:training_organizer/email/send_email_usecase.dart';
+import 'package:training_organizer/email/domain/send_email_usecase.dart';
 import 'package:training_organizer/model/trainee.dart';
 import 'package:training_organizer/model/training_group.dart';
 import 'package:training_organizer/services/date_service.dart';
