bcf29228f753ef59cda4ff7134ec142ebf0b80ae
michaelsandner <michaelsandy@gmx.de>
2026-02-22 15:01:56 +0100

    fix: Correct Gherkin syntax in tests

--- test/unit/model/qualification_test.dart
+++ test/unit/model/qualification_test.dart
@@ -19,169 +19,204 @@ import 'package:training_organizer/model/qualifications/wasserretter.dart';
 void main() {
   group('qualification', () {
     group('Given pirate', () {
-      test('Then type should be pirate', () {
-        final piratequalification = Pirat(null);
+      group('When the qualification is created', () {
+        test('Then type should be pirate', () {
+          final piratequalification = Pirat(null);
 
-        expect(piratequalification.name, 'Pirat');
-        expect(piratequalification.shortName, 'Seeräuber');
-        expect(piratequalification.fullName, 'Seeräuber');
+          expect(piratequalification.name, 'Pirat');
+          expect(piratequalification.shortName, 'Seeräuber');
+          expect(piratequalification.fullName, 'Seeräuber');
+        });
       });
     });
 
     group('Given bronze', () {
-      test('Then type should be bronze', () {
-        final piratequalification = Bronze(null);
+      group('When the qualification is created', () {
+        test('Then type should be bronze', () {
+          final piratequalification = Bronze(null);
 
-        expect(piratequalification.name, 'Bronze');
-        expect(piratequalification.shortName, 'Bronze');
-        expect(piratequalification.fullName, 'Schwimmabzeichen Bronze');
+          expect(piratequalification.name, 'Bronze');
+          expect(piratequalification.shortName, 'Bronze');
+          expect(piratequalification.fullName, 'Schwimmabzeichen Bronze');
+        });
       });
     });
 
     group('Given silber', () {
-      test('Then type should be silber', () {
-        final piratequalification = Silber(null);
+      group('When the qualification is created', () {
+        test('Then type should be silber', () {
+          final piratequalification = Silber(null);
 
-        expect(piratequalification.name, 'Silber');
-        expect(piratequalification.shortName, 'Silber');
-        expect(piratequalification.fullName, 'Schwimmabzeichen Silber');
+          expect(piratequalification.name, 'Silber');
+          expect(piratequalification.shortName, 'Silber');
+          expect(piratequalification.fullName, 'Schwimmabzeichen Silber');
+        });
       });
     });
 
     group('Given gold', () {
-      test('Then type should be gold', () {
-        final piratequalification = Gold(null);
+      group('When the qualification is created', () {
+        test('Then type should be gold', () {
+          final piratequalification = Gold(null);
 
-        expect(piratequalification.name, 'Gold');
-        expect(piratequalification.shortName, 'Gold');
-        expect(piratequalification.fullName, 'Schwimmabzeichen Gold');
+          expect(piratequalification.name, 'Gold');
+          expect(piratequalification.shortName, 'Gold');
+          expect(piratequalification.fullName, 'Schwimmabzeichen Gold');
+        });
       });
     });
 
     group('Given rettungsschwimmer bronze', () {
-      test('Then type should be rettungsschwimmer bronze', () {
-        final piratequalification = RsBronze(null);
+      group('When the qualification is created', () {
+        test('Then type should be rettungsschwimmer bronze', () {
+          final piratequalification = RsBronze(null);
 
-        expect(piratequalification.name, 'RettungsschwimmerBronze');
-        expect(piratequalification.shortName, 'RS Bronze');
-        expect(piratequalification.fullName, 'Rettungsschwimmabzeichen Bronze');
+          expect(piratequalification.name, 'RettungsschwimmerBronze');
+          expect(piratequalification.shortName, 'RS Bronze');
+          expect(
+              piratequalification.fullName, 'Rettungsschwimmabzeichen Bronze');
+        });
       });
     });
 
     group('Given rettungsschwimmer silber', () {
-      test('Then type shouldd be rettungsschwimmer silber', () {
-        final piratequalification = RsSilber(null);
+      group('When the qualification is created', () {
+        test('Then type should be rettungsschwimmer silber', () {
+          final piratequalification = RsSilber(null);
 
-        expect(piratequalification.name, 'RettungsschwimmerSilber');
-        expect(piratequalification.shortName, 'RS Silber (< 2 Jahre)');
-        expect(piratequalification.fullName, 'Rettungsschwimmabzeichen Silber');
+          expect(piratequalification.name, 'RettungsschwimmerSilber');
+          expect(piratequalification.shortName, 'RS Silber (< 2 Jahre)');
+          expect(
+              piratequalification.fullName, 'Rettungsschwimmabzeichen Silber');
+        });
       });
     });
 
     group('Given rettungsschwimmer gold', () {
-      test('Then type should be rettungsschwimmer gold', () {
-        final piratequalification = RsGold(null);
+      group('When the qualification is created', () {
+        test('Then type should be rettungsschwimmer gold', () {
+          final piratequalification = RsGold(null);
 
-        expect(piratequalification.name, 'RettungsschwimmerGold');
-        expect(piratequalification.shortName, 'RS Gold');
-        expect(piratequalification.fullName, 'Rettungsschwimmabzeichen Gold');
+          expect(piratequalification.name, 'RettungsschwimmerGold');
+          expect(piratequalification.shortName, 'RS Gold');
+          expect(piratequalification.fullName, 'Rettungsschwimmabzeichen Gold');
+        });
       });
     });
 
     group('Given rettungsschwimmer im Wasserrettungsdienst', () {
-      test('Then type should be rettungsschwimmer im Wasserrettungsdienst', () {
-        final piratequalification = RSiWRD(null);
+      group('When the qualification is created', () {
+        test('Then type should be rettungsschwimmer im Wasserrettungsdienst',
+            () {
+          final piratequalification = RSiWRD(null);
 
-        expect(piratequalification.name, 'RSiWRD');
-        expect(piratequalification.shortName, 'RSiWRD');
-        expect(piratequalification.fullName,
-            'Rettungsschwimmer*in im Wasserrettungsdient');
+          expect(piratequalification.name, 'RSiWRD');
+          expect(piratequalification.shortName, 'RSiWRD');
+          expect(piratequalification.fullName,
+              'Rettungsschwimmer*in im Wasserrettungsdient');
+        });
       });
     });
 
     group('Given san', () {
-      test('Then type should be san', () {
-        final piratequalification = San(null);
+      group('When the qualification is created', () {
+        test('Then type should be san', () {
+          final piratequalification = San(null);
 
-        expect(piratequalification.name, 'San');
-        expect(piratequalification.shortName, 'San');
-        expect(piratequalification.fullName, 'Sanitätsdiensthelfer*in');
+          expect(piratequalification.name, 'San');
+          expect(piratequalification.shortName, 'San');
+          expect(piratequalification.fullName, 'Sanitätsdiensthelfer*in');
+        });
       });
     });
 
     group('Given wasserretter', () {
-      test('Then type should be wasserretter', () {
-        final piratequalification = Wasserretter(null);
+      group('When the qualification is created', () {
+        test('Then type should be wasserretter', () {
+          final piratequalification = Wasserretter(null);
 
-        expect(piratequalification.name, 'Wasserretter');
-        expect(piratequalification.shortName, 'Wasserretter');
-        expect(piratequalification.fullName, 'Wasserretter*in');
+          expect(piratequalification.name, 'Wasserretter');
+          expect(piratequalification.shortName, 'Wasserretter');
+          expect(piratequalification.fullName, 'Wasserretter*in');
+        });
       });
     });
 
     group('Given ausbildungsassistent', () {
-      test('Then type should be ausbildungsassistent', () {
-        final piratequalification = Assistent(null);
+      group('When the qualification is created', () {
+        test('Then type should be ausbildungsassistent', () {
+          final piratequalification = Assistent(null);
 
-        expect(piratequalification.name, 'Ausbildungsassistent');
-        expect(piratequalification.shortName, 'Assistent');
-        expect(piratequalification.fullName, 'Ausbildungsassistent*in');
+          expect(piratequalification.name, 'Ausbildungsassistent');
+          expect(piratequalification.shortName, 'Assistent');
+          expect(piratequalification.fullName, 'Ausbildungsassistent*in');
+        });
       });
     });
 
     group('Given ausbilder s1', () {
-      test('Then type should be s1', () {
-        final piratequalification = AusbilderS1(null);
+      group('When the qualification is created', () {
+        test('Then type should be s1', () {
+          final piratequalification = AusbilderS1(null);
 
-        expect(piratequalification.name, 'AusbilderS1');
-        expect(piratequalification.shortName, 'AusbilderS1');
-        expect(piratequalification.fullName, 'Ausbilder*in S Stufe 1');
+          expect(piratequalification.name, 'AusbilderS1');
+          expect(piratequalification.shortName, 'AusbilderS1');
+          expect(piratequalification.fullName, 'Ausbilder*in S Stufe 1');
+        });
       });
     });
 
     group('Given ausbilder s2', () {
-      test('Then type should be s2', () {
-        final piratequalification = AusbilderS2(null);
+      group('When the qualification is created', () {
+        test('Then type should be s2', () {
+          final piratequalification = AusbilderS2(null);
 
-        expect(piratequalification.name, 'AusbilderS2');
-        expect(piratequalification.shortName, 'AusbilderS2');
-        expect(piratequalification.fullName, 'Ausbilder*in S Stufe 2');
+          expect(piratequalification.name, 'AusbilderS2');
+          expect(piratequalification.shortName, 'AusbilderS2');
+          expect(piratequalification.fullName, 'Ausbilder*in S Stufe 2');
+        });
       });
     });
 
     group('Given ausbilder r1', () {
-      test('Then type should be r1', () {
-        final piratequalification = AusbilderR1(null);
+      group('When the qualification is created', () {
+        test('Then type should be r1', () {
+          final piratequalification = AusbilderR1(null);
 
-        expect(piratequalification.name, 'AusbilderR1');
-        expect(piratequalification.shortName, 'AusbilderR1');
-        expect(piratequalification.fullName, 'Ausbilder*in R Stufe 1');
+          expect(piratequalification.name, 'AusbilderR1');
+          expect(piratequalification.shortName, 'AusbilderR1');
+          expect(piratequalification.fullName, 'Ausbilder*in R Stufe 1');
+        });
       });
     });
 
     group('Given ausbilder r2', () {
-      test('Then type should be r2', () {
-        final piratequalification = AusbilderR2(null);
+      group('When the qualification is created', () {
+        test('Then type should be r2', () {
+          final piratequalification = AusbilderR2(null);
 
-        expect(piratequalification.name, 'AusbilderR2');
-        expect(piratequalification.shortName, 'AusbilderR2');
-        expect(piratequalification.fullName, 'Ausbilder*in R Stufe 2');
+          expect(piratequalification.name, 'AusbilderR2');
+          expect(piratequalification.shortName, 'AusbilderR2');
+          expect(piratequalification.fullName, 'Ausbilder*in R Stufe 2');
+        });
       });
     });
 
     group('Given gruppenleiter', () {
-      test('Then type should be gruppenleiter', () {
-        final piratequalification = Gruppenleiter(null);
+      group('When the qualification is created', () {
+        test('Then type should be gruppenleiter', () {
+          final piratequalification = Gruppenleiter(null);
 
-        expect(piratequalification.name, 'Gruppenleiter');
-        expect(piratequalification.shortName, 'Gruppenleiter');
-        expect(piratequalification.fullName, 'Gruppenleiter*in');
+          expect(piratequalification.name, 'Gruppenleiter');
+          expect(piratequalification.shortName, 'Gruppenleiter');
+          expect(piratequalification.fullName, 'Gruppenleiter*in');
+        });
       });
     });
 
-    group('toJson', () {
-      group('when date is null', () {
-        test('shouldd store null', () {
+    group('Given a Bronze qualification', () {
+      group('When toJson is called with null date', () {
+        test('Then it should store null', () {
           final bronzequalification = Bronze(null);
           final expectedJson = {
             'name': 'Bronze',
@@ -192,8 +227,8 @@ void main() {
         });
       });
 
-      group('when date is valid', () {
-        test('shouldd store date', () {
+      group('When toJson is called with valid date', () {
+        test('Then it should store the date', () {
           final bronzequalification = Bronze(DateTime(2020, 9, 9));
 
           final expectedJson = {
@@ -208,7 +243,7 @@ void main() {
 
     group('Given is older than two years isUpToDate', () {
       group('When isUpToDate is called', () {
-        test('Then shouldd returns false', () {
+        test('Then it should return false', () {
           final date = DateTime(DateTime.now().year - 2, DateTime.now().month,
               DateTime.now().day - 2);
           final rsSilber = RsSilber(date);
@@ -219,7 +254,7 @@ void main() {
     });
     group('Given is younger than two years', () {
       group('When isUpToDate is called', () {
-        test('returns true', () {
+        test('Then it should return true', () {
           final date = DateTime(DateTime.now().year - 2, DateTime.now().month,
               DateTime.now().day + 1);
           final rsSilber = RsSilber(date);
--- test/unit/services/date_service_test.dart
+++ test/unit/services/date_service_test.dart
@@ -2,22 +2,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:training_organizer/services/date_service.dart';
 
 void main() {
-  group('DateService', () {
-    test('ParseToDate', () {
-      String inputDate = '07.03.2023';
+  group('Given a date string in German format', () {
+    group('When parseToDate is called', () {
+      test('Then it should return the correct DateTime', () {
+        String inputDate = '07.03.2023';
 
-      DateTime outputDate = DateService.parseToDate(inputDate);
+        DateTime outputDate = DateService.parseToDate(inputDate);
 
-      expect(outputDate.year, 2023);
-      expect(outputDate.month, 3);
-      expect(outputDate.day, 7);
+        expect(outputDate.year, 2023);
+        expect(outputDate.month, 3);
+        expect(outputDate.day, 7);
+      });
     });
-    test('FormatToGerman', () {
-      String inputDate = '2023-03-07';
+  });
 
-      String output = DateService.formatToGerman(inputDate);
+  group('Given a date string in ISO format', () {
+    group('When formatToGerman is called', () {
+      test('Then it should return the date in German format', () {
+        String inputDate = '2023-03-07';
 
-      expect(output, '07.03.2023');
+        String output = DateService.formatToGerman(inputDate);
+
+        expect(output, '07.03.2023');
+      });
     });
   });
 }
