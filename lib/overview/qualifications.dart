857dec424ed5ffca7b5b0a67183f9a10f76339b0
michaelsandner <michaelsandy@gmx.de>
2026-02-23 14:56:56 +0100

    refactor: Extract overlay to new file

--- lib/overview/qualifications.dart
+++ lib/overview/qualifications.dart
@@ -3,51 +3,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:training_organizer/model/qualifications/qualification.dart';
 import 'package:training_organizer/model/trainee.dart';
 
-class QualificationOverlay extends StatelessWidget {
-  final Trainee trainee;
-  final Widget child;
-  const QualificationOverlay({
-    required this.child,
-    required this.trainee,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: () => showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Ausbildungen'),
-              content: SizedBox(
-                height: 200,
-                child: Column(
-                  children:
-                      List.generate(trainee.qualifications.length, (index) {
-                    final currentqualification = trainee.qualifications[index];
-
-                    return Row(
-                      children: [
-                        currentqualification.icon,
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        if (currentqualification.date != null)
-                          Text(currentqualification.date!.year.toString()),
-                      ],
-                    );
-                  }),
-                ),
-              ),
-            );
-          }),
-      child: child,
-    );
-  }
-}
-
 class Qualifications extends StatelessWidget {
   final Trainee trainee;
   const Qualifications({
